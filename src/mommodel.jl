358fa0d0d5d6c12e93c1f08b5c8845d8e20ce812
mourrain <bernard.mourrain@inria.fr>
2020-04-30 23:45:41 +0200

    add SDP variables

--- src/mommodel.jl
+++ src/mommodel.jl
@@ -13,49 +13,51 @@ end
     
 #----------------------------------------------------------------------
 # Define a moment model
-function Model(X, d::Int64; nu::Int64=1, w::Vector = [(-1)^(k-1) for k in 1:nu],   kwargs...)
+function Model(X, d::Int64; nu::Int64=1,   kwargs...)
 
-    m = JuMP.Model(kwargs...)
+    M = JuMP.Model(kwargs...)
 
-    m[:nu] = nu
-    m[:w] = w
-    m[:variables] = X
-    m[:degree] = d
+    M[:nu] = nu
+    M[:variables] = X
+    M[:degree] = d
     
     B = monomials(X,seq(0:d))
     N = length(B)
-    m[:basis] = B
+    M[:basis] = B
 
-    m[:monomials] = DynamicPolynomials.Monomial{true}[]
-    m[:index] = Dict{DynamicPolynomials.Monomial{true},Int64}()
+    M[:index] = Dict{DynamicPolynomials.Monomial{true},Tuple{Int64,Int64}}()
+
+    M[:monomials] = DynamicPolynomials.Monomial{true}[]
+
+    M[:H] = []
+    for k in 1:nu
+        push!(M[:H], @variable(M, [1:N,1:N], SDP))
+    end
 
     # Hankel structure
-    c = 0
     for i in 1:N
-        for j  in 1:N
-            mn = B[i]*B[j]
-	    if !haskey(m[:index], mn)
-                c += 1
-                m[:index][mn] = c
-                push!(m[:monomials], mn)
-	    end
-        end
-    end
-    s = length(m[:monomials])
-    @variable(m, y[1:s,1:nu])
-    m[:moments] = y
-
-    for k in 1:nu 
-        H = [ y[m[:index][B[i]*B[j]],k] for i in 1:N, j in 1:N]
-        @SDconstraint(m, Symmetric(H) >= zeros(N,N))
+        for j in 1:N
+            mn = M[:basis][i]*M[:basis][j]
+	        if !haskey(M[:index], mn)
+	            M[:index][mn] = (i,j)
+                    push!(M[:monomials], mn)
+	        else
+                    id = get(M[:index], mn, (0,0))
+	            if (i != id[2]) || (j != id[1])
+                        for k in 1:nu
+	                    @constraint(m, M[:H][k][i,j]-M[:H][k][id...] == 0)
+                        end
+                    end
+	        end
+	end
     end
 
-    return MOM.Model(m)
+    return MOM.Model(M)
 end
 
 function Model(X, d::Int64, optimizer; kwargs...)
     M = MOM.Model(X,d; kwargs...)
-    set_optimizer(M, optimizer)
+    set_optimizer(M.model, optimizer)
     return M
 end
 
@@ -65,11 +67,11 @@ function add_constraint_zero(M::MOM.Model, eq)
     p = eq*one(Polynomial{true,Float64})
     X = M[:variables]
     L = monomials(X,seq(0:2*M[:degree]-maxdegree(p)))
+
     for mn in L
-        q = p*mn*one(Float64)
         for k in 1:M[:nu]
             @constraint(M.model,
-                        sum(t.α*M[:moments][M[:index][t.x],k] for t in q) == 0)
+                        sum(t.α*M[:H][k][M[:index][t.x]...] for t in p*mn)==0)
         end
     end
 end
@@ -82,7 +84,7 @@ function add_constraint_zero(M::MOM.Model, idx::Vector{Int64}, Veq)
     L = monomials(X,seq(0:2*M[:degree]-d0))
     for mn in L
         @constraint(M.model,
-                    sum(sum(t.α*M[:moments][M[:index][t.x],idx[k]] for t in P[k]*mn) for k in 1:length(idx)) == 0)
+                    sum(sum(t.α*M[:H][idx[k]][M[:index][t.x]...] for t in P[k]*mn) for k in 1:length(idx)) == 0)
     end
 end
 
@@ -100,11 +102,15 @@ function add_constraint_nneg(M::MOM.Model, pos)
                         sum(t.α*M[:moments][M[:index][t.x],k] for t in p)>=0)
         end
     else
-        for k in 1:M[:nu]
-            P = [ sum(t.α*M[:moments][M[:index][t.x*L[i]*L[j]],k]
-                      for t in p)
-                  for i in 1:N, j in 1:N ]
-            @SDconstraint(M.model, P >= zeros(N,N))
+        P  = @variable(m, [1:N,1:N], SDP)
+        for i in 1:N
+            for j  in 1:i
+                mn = L[i]*L[j]
+                for k in 1:M[:nu]
+                    @constraint(M.model,
+                                P[i,j]-sum(t.α*M[:H][k][M[:index][t.x*mn]...] for t in p) == 0)
+                end
+            end
         end
     end
 end
@@ -118,16 +124,18 @@ function add_constraint_nneg(M::MOM.Model, idx::Vector{Int64}, Veq)
     N = length(L)
     if N == 1
         @constraint(M.model,
-                    sum(sum(t.α*M[:moments][M[:index][t.x],idx[k]] for t in p[k]) for k in 1:length(idx)) >=0)
+                    sum(sum(t.α*M[:H][idx[k]][M[:index][t.x]...] for t in p[k]) for k in 1:length(idx)) >=0)
     else
-
-        P = [ sum(sum(t.α*M[:moments][M[:index][t.x*L[i]*L[j]],idx[k]]
-                      for t in p[k]) for k in 1:length(idx)) 
-              for i in 1:N, j in 1:N ]
-        @SDconstraint(M.model, P >= zeros(N,N))
+        P  = @variable(m, [1:N,1:N], SDP)
+        for i in 1:N
+            for j  in 1:i
+                mn = L[i]*L[j]
+                @constraint(M.model,
+                            P[i,j]-sum(sum(t.α*M[:H][idx[k]][M[:index][t.x*L[i]*L[j]]...] for t in p[k]) for k in 1:length(idx)) == 0 ) 
+            end
+        end
     end
 end
-
 
 end  #module MOM
 
