e032c1f4a48e58eb28a2892ee07e4fa3764db6a0
mourrain <bernard.mourrain@inria.fr>
2023-03-07 16:43:29 +0100

    add docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,29 @@
-using Documenter, DocumenterMarkdown
+using Documenter
 using MomentTools, JuMP
 
 dir = "mrkd"
 Expl = map(file -> joinpath("expl", file),
-           filter(x ->endswith(x, "md"), readdir(dir*"/expl")))
-Code = map(file -> joinpath("code", file), filter(x ->endswith(x, "md"), readdir(dir*"/code")))
+           filter(x ->endswith(x, ".md"), readdir(dir*"/expl")))
+Code = map(file -> joinpath("code", file), filter(x ->endswith(x, ".md"), readdir(dir*"/code")))
 
 makedocs(
     sitename = "MomentTools",
-    format = Documenter.HTML(prettyurls = false),
-    authors = "B. Mourrain",
+#    format = Documenter.HTML(prettyurls = false),
+    authors = "L. Baldi, B. Mourrain",
     modules = [MomentTools],
-    build = "html",
-    source = dir,
+    build = "MomentTools.jl/docs",
+    source = "mrkd",
     pages = Any[
         "Home" => "index.md",
         "Functions & types" => Code,
         "Example" => Expl,
         "About the package"  => "package.md",
     ],
-    doctest = false
+    repo = "https://github.com/AlgebraicGeometricModeling/MomentTools.jl",
+    doctest = false,
 )
 
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo = "gitlab.inria.fr/AlgebraicGeometricModeling/MomentTools.jl.git"
+    repo = "github.com/AlgebraicGeometricModeling/MomentTools.jl.git"
 )
