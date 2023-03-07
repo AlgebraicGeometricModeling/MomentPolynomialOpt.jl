2de210d342467dfe57b3d89d6de667124e8017aa
mourrain <bernard.mourrain@inria.fr>
2023-03-07 16:48:15 +0100

    add expl

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,11 @@ using MomentTools, JuMP
 dir = "mrkd"
 Expl = map(file -> joinpath("expl", file),
            filter(x ->endswith(x, ".md"), readdir(dir*"/expl")))
-Code = map(file -> joinpath("code", file), filter(x ->endswith(x, ".md"), readdir(dir*"/code")))
+Code = map(file -> joinpath("code", file),
+           filter(x ->endswith(x, ".md"), readdir(dir*"/code")))
 
 makedocs(
     sitename = "MomentTools",
-#    format = Documenter.HTML(prettyurls = false),
     authors = "L. Baldi, B. Mourrain",
     modules = [MomentTools],
     build = "MomentTools.jl/docs",
